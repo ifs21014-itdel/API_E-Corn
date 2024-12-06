e55c0dac81e60090829ecaa455f779d0bc1b661f
unknown <corrysitorus03@gmail.com>
2024-12-06 12:55:25 +0700

    features

--- models/featuresModel.js
+++ models/featuresModel.js
@@ -1,9 +1,9 @@
 const db = require('../config/db');
 
 const Features = {
-  createFeature: async (title, description, imageUrl) => {
-    const query = `INSERT INTO features (title, description, image_url) VALUES (?, ?, ?)`;
-    const [result] = await db.execute(query, [title, description, imageUrl]);
+  createFeature: async (title, description, image) => {
+    const query = `INSERT INTO features (title, description, image) VALUES (?, ?, ?)`;
+    const [result] = await db.execute(query, [title, description, image]);
     return result;
   },
 
@@ -19,12 +19,26 @@ const Features = {
     return rows;
   },
 
-  updateFeature: async (id, title, description, imageUrl) => {
-    const query = `
-      UPDATE features 
-      SET title = ?, description = ?, image_url = ?, updated_at = CURRENT_TIMESTAMP 
-      WHERE id = ?`;
-    const [result] = await db.execute(query, [title, description, imageUrl, id]);
+  updateFeature: async (id, title, description, image) => {
+    let query, params;
+
+    // Jika ada gambar baru, masukkan juga ke dalam query
+    if (image) {
+      query = `
+        UPDATE features 
+        SET title = ?, description = ?, image = ?, updated_at = CURRENT_TIMESTAMP 
+        WHERE id = ?`;
+      params = [title, description, image, id];
+    } else {
+      // Jika tidak ada gambar, update hanya title dan description
+      query = `
+        UPDATE features 
+        SET title = ?, description = ?, updated_at = CURRENT_TIMESTAMP 
+        WHERE id = ?`;
+      params = [title, description, id];
+    }
+
+    const [result] = await db.execute(query, params);
     return result;
   },
 
