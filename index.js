e749db0200964f2a2f7df90cc5954c033892b9bd
unknown <corrysitorus03@gmail.com>
2024-12-16 14:30:50 +0700

    seeder

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173', 
+    origin: 'http://localhost:5176', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true, 
   })
